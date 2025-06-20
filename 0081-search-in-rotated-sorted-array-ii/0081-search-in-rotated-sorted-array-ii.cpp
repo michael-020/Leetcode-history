b6f893eba782bedf868b18a77f82828f0b5c213a
Michael <143117803+michael-020@users.noreply.github.com>
2025-06-20 17:08:03 +0530

    Time: 3 ms (3.03%), Space: 16.8 MB (100%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int low = 0, high = n-1;
-        while(low <= high){
-            int mid = (low+high)/2;
-            if(nums[mid] == target)
-                return true;
-            else if(nums[low] <= nums[mid]){
-                if(nums[mid] >= target && target >= nums[low]){
-                    high = mid-1;
+    bool search(vector<int>& arr, int target) {
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            //if mid points the target
+            if (arr[mid] == target) return true;
+
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+
+            //if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target && target <= arr[mid]) {
+                    //element exists in the left part:
+                    high = mid - 1;
                 }
-                else{
-                    low = mid+1;
+                else {
+                    //element exists in the right part:
+                    low = mid + 1;
                 }
             }
-            else{
-                if(nums[mid] <= target && target <= nums[high]){
-                    low = mid+1;
+            else { //if right part is sorted:
+                if (arr[mid] <= target && target <= arr[high]) {
+                    //element exists in the right part:
+                    low = mid + 1;
                 }
-                else{
-                    high = mid-1;
+                else {
+                    //element exists in the left part:
+                    high = mid - 1;
                 }
             }
         }
