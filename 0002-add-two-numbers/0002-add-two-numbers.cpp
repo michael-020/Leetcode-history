ce230e730fbc38cbfd910268a47c62f800c91a40
Michael <143117803+michael-020@users.noreply.github.com>
2026-03-04 22:14:26 +0530

    Time: 7 ms (7.33%), Space: 77.2 MB (12.87%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -16,70 +16,26 @@ public:
         int sum = 0;
         int carry = 0;
 
-        while(l1 && l2){
-            sum = l1->val + l2->val + carry;
+        while(l1 || l2 || carry){
+            sum = carry;
+
+            if(l1){
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if(l2){
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
             int digit = sum % 10;
             carry = sum / 10;
 
-            if(carry == 0){
-                ListNode* newNode = new ListNode(sum);
-                tail->next = newNode;
-                tail = tail->next;
-            }
-            else {
-                ListNode* newNode = new ListNode(digit);
-                tail->next = newNode;
-                tail = tail->next;
-            }
-            l1 = l1->next;
-            l2 = l2->next;
-        }
-
-        if(l1){
-            while(l1){
-                sum = l1->val + carry;
-                int digit = sum % 10;
-                carry = sum / 10;
-
-                if(carry == 0){
-                    ListNode* newNode = new ListNode(sum);
-                    tail->next = newNode;
-                    tail = tail->next;
-                }
-                else {
-                    ListNode* newNode = new ListNode(digit);
-                    tail->next = newNode;
-                    tail = tail->next;
-                }
-                l1 = l1->next;
-            }
-        }
-        else {
-            while(l2){
-                sum = l2->val + carry;
-                int digit = sum % 10;
-                carry = sum / 10;
-
-                if(carry == 0){
-                    ListNode* newNode = new ListNode(sum);
-                    tail->next = newNode;
-                    tail = tail->next;
-                }
-                else {
-                    ListNode* newNode = new ListNode(digit);
-                    tail->next = newNode;
-                    tail = tail->next;
-                }
-                l2 = l2->next;
-            }
-        }
-
-        if(carry != 0){
-            ListNode* newNode = new ListNode(carry);
+            ListNode* newNode = new ListNode(digit);
             tail->next = newNode;
             tail = tail->next;
         }
-
         return dummy->next;
     }
 };
