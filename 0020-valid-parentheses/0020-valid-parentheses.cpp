4a4dfb4d687f6ae9a6f3cd5d598b95c45ce70c4f
Michael <143117803+michael-020@users.noreply.github.com>
2026-03-10 21:06:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0; i<s.size(); i++){
+            char ch = s[i];
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }
+            else{
+                if(!st.empty()){
+                    if(ch == ')' && st.top() == '('){
+                        st.pop();
+                    }
+                    else if(ch == ']' && st.top() == '['){
+                        st.pop();
+                    }
+                    else if(ch == '}' && st.top() == '{'){
+                        st.pop();
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
