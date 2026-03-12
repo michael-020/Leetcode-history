5442ee120b26effca51e7c317dbad4abf7f0aa5e
Michael <143117803+michael-020@users.noreply.github.com>
2026-03-13 01:51:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,61 +1,27 @@
 class Solution {
 public:
-    vector<int> nextSmaller(vector<int> nums, int n){
-        stack<int> st;
-        vector<int> ans(n);
-        for(int i=n-1; i>=0; i--){
-            while(!st.empty() && nums[st.top()] >= nums[i]){
-                st.pop();
-            }
-
-            if(st.empty()){
-                ans[i] = -1;
-            }
-            else {
-                ans[i] = st.top();
-            }
-            st.push(i);
-        }
-
-        return ans;
-    }
-
-    vector<int> prevSmaller(vector<int> nums, int n){
-        vector<int> ans(n);
-        stack<int> st;
-
-        for(int i=0; i<n; i++){
-            while(!st.empty() && nums[st.top()] >= nums[i]){
-                st.pop();
-            }
-
-            if(st.empty())
-                ans[i] = -1;
-            else
-                ans[i] = st.top();
-
-            st.push(i);
-        }
-        return ans;
-    }
-
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int> prev = prevSmaller(heights, n);
-        vector<int> next = nextSmaller(heights, n);
+        stack<int> st;
+        int maxArea = 0;
+        
+        for(int i=0; i<=n; i++){
+            int currHeight = i==n ? 0 : heights[i];
 
-        for(int &num: next){
-            if(num == -1)
-                num = n;
-        }
+            while(!st.empty() && currHeight < heights[st.top()]){
+                int height = heights[st.top()];
+                st.pop();
 
-        int maxArea = INT_MIN;
+                int width;
+                if(st.empty())
+                    width = i;
+                else
+                    width = i - st.top() - 1;
 
-        for(int i=0; i<n; i++){
-            int length = heights[i];
-            int width = next[i] - prev[i] - 1;
-            int currArea = length * width;
-            maxArea = max(maxArea, currArea);
+                maxArea = max(maxArea, height * width);
+            }
+            
+            st.push(i);
         }
 
         return maxArea;
