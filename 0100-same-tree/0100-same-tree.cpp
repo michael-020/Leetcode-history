15ad38c4a26eff4042127375c4e647c127823c24
Michael <143117803+michael-020@users.noreply.github.com>
2026-03-20 18:05:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,39 +11,14 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<pair<int, int>>& v, const int &index){
-        if(!root){
-            v.push_back({0, -1});
-            return;
-        }
-
-        inorder(root->left, v, 2*index+1);
-
-        v.push_back({root->val, index});
-
-        inorder(root->right, v, 2*index+2);
-    }
-
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(!p && !q)
             return true;
         if(!p || !q)
             return false;
-        
-        vector<pair<int, int>> inorderP;
-        vector<pair<int, int>> inorderQ;
-        const int index = 0, id = 0;
-        inorder(p, inorderP, index);
-        inorder(q, inorderQ, id);
-
-        if(inorderP.size() != inorderQ.size())
+        if(p->val != q->val)
             return false;
 
-        for(int i=0; i<inorderP.size(); i++){
-            if(inorderP[i].first != inorderQ[i].first || inorderP[i].second != inorderQ[i].second)
-                return false;
-        }
-
-        return true;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
